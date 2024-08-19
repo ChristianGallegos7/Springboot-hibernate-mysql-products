e332102f06d0941e49237b07f7d617ea357c3b6e
StarLord🧑‍🎤 <star@lord.es>
2024-08-19 10:26:18 -0500

    Response Status added

--- src/main/java/com/bases/project/services/ProductoService.java
+++ src/main/java/com/bases/project/services/ProductoService.java
@@ -3,7 +3,9 @@ package com.bases.project.services;
 import com.bases.project.entities.Producto;
 import com.bases.project.repositories.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -21,6 +23,7 @@ public class ProductoService {
         return this.productoRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     public Producto create(Producto producto){
         return this.productoRepository.save(producto);
     }
@@ -32,6 +35,7 @@ public class ProductoService {
         return this.productoRepository.save(producto);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public String delete(Long id){
         this.productoRepository.deleteById(id);
         return "Product deleted successfully!";
